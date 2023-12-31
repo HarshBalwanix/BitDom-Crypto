e3c18ca4514bc2d564a9e99d105aa8621c05e794
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-12-31 15:54:54 +0530

    Created Exchanges section

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -4,18 +4,21 @@ import axios from "axios";
 import { BaseUrl } from "./baseurl";
 import { useState } from "react";
 import Loader from "./Loader";
+import coin from "../coin.png";
 
 const Exchanges = () => {
-  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(true);
+  const [exchanges, setExchanges] = useState([]);
+
   useEffect(() => {
     const getExchangesData = async () => {
       const { data } = await axios.get(`${BaseUrl}/exchanges`);
       console.log(data);
+      setExchanges(data);
       setLoading(false);
     };
     getExchangesData();
-  });
+  }, []);
   return (
     <>
       {loading ? (
@@ -23,7 +26,22 @@ const Exchanges = () => {
       ) : (
         <>
           <Header />
-          <div></div>
+          <div>
+            {exchanges.map((item, i) => {
+              return (
+                <div className="ex-cards flex items-center justify-evenly mt-20">
+                  <div className="image">
+                    <img src={item.image} height={"80px"} alt="loading error" />
+                  </div>
+                  <div className="name w-20">{item.name}</div>
+                  <div className="price w-20">
+                    {item.trade_volume_24h_btc.toFixed(2)}
+                  </div>
+                  <div className="rank w-20">{item.trust_score_rank}</div>
+                </div>
+              );
+            })}
+          </div>
         </>
       )}
     </>
