ec5b31645a5d2581c6718c49ee44c4e0834f71f7
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-01-04 23:25:15 +0530

    Made header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 import { SiBitcoinsv } from "react-icons/si";
 const Header = () => {
   return (
-    <div className="flex items-center justify-evenly text-white bg-[#0e062f] py-4">
-      <div className="flex items-center">
-        <h1 className="text-4xl">BITDOM</h1>
+    <div className="navbar flex items-center justify-evenly  bg-[#0e062f] p-0 m-0 sm:py-4 h-16 ">
+      <div className="logo flex items-center">
+        <h1 className="text-xl sm:text-4xl">BITDOM</h1>
         <SiBitcoinsv color="orange" size={"35"} />
       </div>
       <ul className="flex ">
         <li>
-          <Link className="ml-60 text-xl" to="/">
+          <Link className="ml-2 text-lg sm:ml-60 sm:text-xl" to="/">
             Home
           </Link>
         </li>
         <li>
-          <Link className="ml-60 text-xl" to="/coins">
+          <Link className="ml-2 text-lg sm:ml-60 sm:text-xl" to="/coins">
             Market
           </Link>
         </li>
         <li>
           <Link
-            className="ml-60 text-xl"
+            className="ml-2 text-lg sm:ml-60 sm:text-xl"
             to="https://www.linkedin.com/company/blockchain-club-vitb/mycompany"
             target="_blank"
           >
